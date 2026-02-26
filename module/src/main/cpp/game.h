cdfaa478eb33cc1c9afc9595c27d76508a05645f
wingchong18 <167405733+wingchong18@users.noreply.github.com>
2026-02-26 17:09:50 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.sea.miniworldroyale.sparkgame"
+#define GamePackageName "com.mobile.legends"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
